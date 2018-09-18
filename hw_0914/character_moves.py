49802452a30d4ffef3522edf247c88dad8f0e6fe
E323Com <E323@E323.com>
2018-09-18 15:18:50 +0900

    added command 'quit the process'

--- hw_0914/character_moves.py
+++ hw_0914/character_moves.py
@@ -1,15 +1,19 @@
 from pico2d import *
 import math
-open_canvas()
 
-grass = load_image('grass.png')
-character = load_image('run_animation.png')
+def handle_events():
+    
+    global running
+    
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            running = False
 
-x=0
-y=90
-frame=0
 def GO_RIGHT(grass, character, x, y, frame):
-    while(x < 800 - 10):
+    while(x < 800 - 10 and running):
         clear_canvas()
         grass.draw(400, 30)
         character.clip_draw(frame*100, 0, 100, 100, x, y)
@@ -18,9 +22,11 @@ def GO_RIGHT(grass, character, x, y, frame):
         update_canvas()
         x = x+10
         delay(0.05)
+        handle_events()
     return x
 def GO_UP(grass, character, x, y, frame):
-    while(y<600 - 30):
+    global running
+    while(y<600 - 30 and running):
         clear_canvas()
         grass.draw(400, 30)
         character.clip_draw(frame * 100, 0, 100, 100, x, y)
@@ -29,9 +35,10 @@ def GO_UP(grass, character, x, y, frame):
         update_canvas()
         y = y + 10
         delay(0.05)
+        handle_events()
     return y
 def GO_LEFT(grass, character, x, y, frame):
-    while(x> 0 + 10):
+    while(x> 0 + 10 and running):
         clear_canvas()
         grass.draw(400, 30)
         character.clip_draw(frame * 100, 0, 100, 100, x, y)
@@ -40,9 +47,10 @@ def GO_LEFT(grass, character, x, y, frame):
         update_canvas()
         x = x - 10
         delay(0.05)
+        handle_events()
     return x
 def GO_DOWN(grass, character, x, y, frame):
-    while(y> 90):
+    while(y> 90 and running):
         clear_canvas()
         grass.draw(400, 30)
         character.clip_draw(frame * 100, 0, 100, 100, x, y)
@@ -51,6 +59,7 @@ def GO_DOWN(grass, character, x, y, frame):
         update_canvas()
         y = y - 10
         delay(0.05)
+        handle_events()
     return y
 def DRAW_RECT(grass, character, x, y, frame):
 
@@ -66,7 +75,7 @@ def DRAW_RECT(grass, character, x, y, frame):
 def GO_CIRCLE(grass, character, frame, r=160, angle=0):
     PI = 3.14
     x = 0
-    while(x < 180):
+    while(x < 180 and running):
         clear_canvas()
         grass.draw(400, 30)
         character.clip_draw(frame * 100, 0, 100, 100, r * math.sin(angle*PI) + 400, r * math.cos(angle*PI) + 300)
@@ -76,10 +85,20 @@ def GO_CIRCLE(grass, character, frame, r=160, angle=0):
         angle += 1/90
         x += 1
         delay(0.05)
+        handle_events()
     return y
 
+open_canvas()
 
-while(1):
+grass = load_image('grass.png')
+character = load_image('run_animation.png')
+
+x=0
+y=90
+frame=0
+running = True
+
+while running:
     DRAW_RECT(grass, character, x, y, frame)
 
     y = 90
