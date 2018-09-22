e623fcbd5c1189ddc4edb6367e1350ebbdb3e5d0
KHWoo <khw@khw-PC>
2018-09-23 00:19:17 +0900

    waypoint를 이용한 이동 및 waypoint 전부 삭제 기능 추가

--- hw_0921/Class_and_Instance.py
+++ hw_0921/Class_and_Instance.py
@@ -6,42 +6,59 @@ import pdb
 class Boy:
     def __init__(self):
         self.image = load_image('../Pics/run_animation.png')
-        self.x, self.y = random.uniform(30, 500), random.uniform(90, 500)
+        self.x, self.y = random.randint(30, 500), random.randint(90, 500)
         self.frame = random.randint(0, 7)
         self.speed = random.randint(1, 3)
+        self.dstnum = 0 #목적지의 index값을 나타냄 (waypoint 좌표 중, x가 Wp인덱스의 짝수, y가 Wp인덱스의 홀수)
     def draw(self):
         self.image.clip_draw(self.frame * 100, 0, 100, 100, self.x, self.y)
     def run(self):
-        global Mx, My
-        if Mx == self.x and My == self.y:
+        global Wp, IDX
+        #최대index값이 dstnum보다 작거나, 최대index값이 0이면 리턴
+        if IDX <= self.dstnum or IDX == 0 :
+            print('I\'m Out', self.dstnum)
             return
+        #목적지와 좌표가 같아진 경우 다음 좌표로 설정(dstnum값 증가)
+        if Wp[self.dstnum] == self.x and Wp[self.dstnum+1] == self.y:
+            self.dstnum += 2
+            return
+        print(Wp[self.dstnum], self.x)
         #먼저 x또는 y축으로 평행하게 가는 경우를 예외처리
         #y값이 같을 경우 x값만 이동
-        if My == self.y :
-            if Mx < self.x :
+        if Wp[self.dstnum+1] == self.y :
+            if Wp[self.dstnum] < self.x :
                 self.x -= self.speed
-                if self.x < Mx : self.x = Mx
+                if self.x < Wp[self.dstnum] : self.x = Wp[self.dstnum]
                 return
-            if Mx > self.x :
+            if Wp[self.dstnum] > self.x :
                 self.x += self.speed
-                if self.x > Mx : self.x = Mx
+                if self.x > Wp[self.dstnum] : self.x = Wp[self.dstnum]
                 return
         #x값이 같을 경우 y값만 이동
-        if Mx == self.x:
-            if My < self.y :
+        if Wp[self.dstnum] == self.x:
+            if Wp[self.dstnum+1] < self.y :
                 self.y -= self.speed
-                if self.y < My : self.y = My
+                if self.y < Wp[self.dstnum+1] : self.y = Wp[self.dstnum+1]
                 return
-            if My > self.y :
+            if Wp[self.dstnum+1] > self.y :
                 self.y += self.speed
-                if self.y > My : self.y = My
+                if self.y > Wp[self.dstnum+1] : self.y = Wp[self.dstnum+1]
                 return
         #이제 기울기가 0으로 나눠질 수도, 0이 될 수도 없으므로 나머지를 구현한다.
         else :
-            dif_x, dif_y = Mx - self.x, My - self.y  #마우스 좌표와 현재 좌표값의 차이
+
+            dif_x, dif_y = Wp[self.dstnum] - self.x, Wp[self.dstnum+1] - self.y #마우스 좌표와 현재 좌표값의 차이
             dist = math.sqrt(dif_x**2 + dif_y**2)
             self.x += dif_x * self.speed / dist
             self.y += dif_y * self.speed / dist
+            if dif_x < 0 :
+                if Wp[self.dstnum] - self.x > 0 : self.x = Wp[self.dstnum]
+            else :
+                if Wp[self.dstnum] - self.x < 0 : self.x = Wp[self.dstnum]
+            if dif_y < 0 :
+                if Wp[self.dstnum+1] - self.y > 0 : self.y = Wp[self.dstnum+1]
+            else :
+                if Wp[self.dstnum+1] - self.y < 0 : self.y = Wp[self.dstnum+1]
 
 
 
@@ -52,7 +69,7 @@ class Grass:
         self.image.draw(400, 30)
 
 def handle_events():
-    global running, Mx, My
+    global running, Wp, IDX
     events = get_events()
     for eve in events:
         if eve.type == SDL_QUIT:
@@ -60,14 +77,24 @@ def handle_events():
         elif eve.type == SDL_KEYDOWN:
             if eve.key == SDLK_ESCAPE:
                 running = False
-        elif eve.type == SDL_MOUSEMOTION:
-            Mx, My = eve.x, 600 - eve.y
+        elif eve.type == SDL_MOUSEBUTTONDOWN:
+            if eve.button == 1:
+                Wp += [eve.x, 600 - eve.y]
+                IDX += 2
+            elif eve.button == 3:
+                Wp = []
+                IDX = 0
+                for i in boy:
+                    i.dstnum = 0
+
 
 open_canvas()
-Mx, My =100, 100
-running = True
 
+IDX = 0
+running = True
+Wp = []
 boy = [Boy() for i in range(20)]
+#boy = Boy()
 grass = Grass()
 
 while running:
@@ -76,7 +103,9 @@ while running:
     grass.draw()
     for i in boy:
         i.draw()
-        i.run()
+        if IDX != 0 : i.run()
+    #boy.draw()
+    #boy.run()
     update_canvas()
     delay(0.01)
 
