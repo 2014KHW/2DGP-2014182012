42386b56526e4e9722017ff438301885def6b777
KHWoo <khw@khw-PC>
2018-10-04 20:18:28 +0900

    base states of game frame

---
+++ term/Project/main.py
@@ -0,0 +1,20 @@
+def enter():
+    pass
+
+def draw():
+    pass
+
+def handle_events():
+    pass
+
+def update():
+    pass
+
+def exit():
+    pass
+
+def pause():
+    pass
+
+def resume():
+    pass
---
+++ term/Project/menu_state.py
@@ -0,0 +1,22 @@
+import game_framework
+
+def enter():
+    pass
+
+def draw():
+    pass
+
+def handle_events():
+    pass
+
+def update():
+    pass
+
+def exit():
+    pass
+
+def pause():
+    pass
+
+def resume():
+    pass
