93e045a41d0063df1e57f04cb2bab7260e59703f
KHWoo <khw@khw-PC>
2018-11-03 15:20:52 +0900

    json파일 값을 불러오기

--- term/Project/score_state.py
+++ term/Project/score_state.py
@@ -1,5 +1,37 @@
+from pico2d import *
+import game_framework
+import json
+import play_state
+import menu_state
+
+class score:
+    def __init__(self):
+        self.name = 'empty'
+        self.score = 0
+        self.kills = 0
+        self.time = 0
+
 def enter():
-    pass
+    global s_data, s_file, s_list
+    global cur_score
+
+    s_data = []
+    s_file = open('score_table.json')
+    cur_score = json.load(s_file)
+
+    s_list = []
+
+    print(cur_score)
+
+    for l in cur_score['player']:
+        s = score()
+        s.name = l['name']
+        s.score = l['score']
+        s.kills = l['kills']
+        s.time = l['time']
+        s_list += [s]
+
+
 
 def draw():
     pass
@@ -18,3 +50,10 @@ def pause():
 
 def resume():
     pass
+
+if __name__ == '__main__':
+    import sys
+    current_module = sys.modules[__name__]
+    open_canvas()
+    game_framework.run(current_module)
+    close_canvas()
